9242c6c2df7d065c773e6e25cf5424f16bc17e78
Guilherme Zordan <guilhermezordan@outlook.com>
2016-01-28 16:26:03 -0200

    bower and angular

---
+++ www/app.js
@@ -0,0 +1,23 @@
+angular.module('Main', [])
+	.controller( 'mainController', function ( ) {
+		var mainCtrl = this;
+		mainCtrl.img = new Image();
+		mainCtrl.img.src = './img/teste.jpg';
+
+
+    function snapPicture () {
+        navigator.camera.getPicture (onSuccess, onFail, 
+            { quality: 50, destinationType: Camera.DestinationType.DATA_URL});
+
+        //A callback function when snapping picture is success.
+        function onSuccess (imageData) {    
+            mainCtrl.img.src = "data:image/jpeg;base64," + imageData;
+        }
+
+        //A callback function when snapping picture is fail.
+        function onFail (message) {
+            alert ('Error occured: ' + message);
+        }
+    }
+
+	});
